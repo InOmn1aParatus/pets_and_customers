9828af281f5b257d436c40048a984e14946348fd
Matt Kragen <mattdkragen@gmail.com>
2021-06-10 21:59:12 -0700

    Modify for RSpec tests

--- spec/day_care_spec.rb
+++ spec/day_care_spec.rb
@@ -1,58 +1,62 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+require 'rspec'
 require './lib/day_care'
 require './lib/customer'
 require './lib/pet'
 
-class DayCareTest < Minitest::Test
-  def test_it_exists
-    day_care = DayCare.new("The Dog Spot")
-    assert_instance_of DayCare, day_care
+RSpec.describe DayCare do
+  context 'instantiation' do
+    it 'exists' do
+      day_care = DayCare.new("The Dog Spot")
+      expect(day_care).to be_a(DayCare)
+    end
+  
+    it 'has attributes' do
+      day_care = DayCare.new("The Dog Spot")
+      expect(day_care.name).to eq('The Dog Spot')
+    end
+
+    it 'starts with no customers' do
+      day_care = DayCare.new("The Dog Spot")
+      expect(day_care.customers).to eq([])
+    end
   end
 
-  def test_it_has_a_name
-    day_care = DayCare.new("The Dog Spot")
-    assert_equal "The Dog Spot", day_care.name
-  end
+  context 'methods' do
+    it 'can add customers' do
+      day_care = DayCare.new("The Dog Spot")
+      joel = Customer.new("Joel", 2)
+      billy = Customer.new("Billy", 3)
+      day_care.add_customer(joel)
+      day_care.add_customer(billy)
 
-  def test_it_starts_with_no_customers
-    day_care = DayCare.new("The Dog Spot")
-    assert_equal [], day_care.customers
-  end
+      expect(day_care.customers).to eq([joel, billy])
+    end
 
-  def test_it_can_add_customers
-    day_care = DayCare.new("The Dog Spot")
-    joel = Customer.new("Joel", 2)
-    billy = Customer.new("Billy", 3)
-    day_care.add_customer(joel)
-    day_care.add_customer(billy)
-    assert_equal [joel, billy], day_care.customers
-  end
+    it 'can find customer by id' do
+      joel = Customer.new("Joel", 2)
+      billy = Customer.new("Billy", 3)
+      day_care = DayCare.new("The Dog Spot")
+      day_care.add_customer(joel)
+      day_care.add_customer(billy)
 
-  def test_it_can_list_unfed_pets
-    joel = Customer.new("Joel", 2)
-    samson = Pet.new({name: "Samson", type: :dog})
-    lucy = Pet.new({name: "Lucy", type: :cat})
-    joel.adopt(samson)
-    joel.adopt(lucy)
-    billy = Customer.new("Billy", 3)
-    molly = Pet.new({name: "Molly", type: :cat})
-    billy.adopt(molly)
-    day_care = DayCare.new("The Dog Spot")
-    day_care.add_customer(joel)
-    day_care.add_customer(billy)
-    lucy.feed
+      expect(day_care.customer_by_id(2)).to eq(joel)
+    end
 
-    assert_equal [samson, molly], day_care.unfed_pets
-  end
+    it 'can list unfed pets' do
+      joel = Customer.new("Joel", 2)
+      samson = Pet.new({name: "Samson", type: :dog})
+      lucy = Pet.new({name: "Lucy", type: :cat})
+      joel.adopt(samson)
+      joel.adopt(lucy)
+      billy = Customer.new("Billy", 3)
+      molly = Pet.new({name: "Molly", type: :cat})
+      billy.adopt(molly)
+      day_care = DayCare.new("The Dog Spot")
+      day_care.add_customer(joel)
+      day_care.add_customer(billy)
+      lucy.feed
 
-  def test_it_can_find_customer_by_id
-    joel = Customer.new("Joel", 2)
-    joel.charge(10)
-    billy = Customer.new("Billy", 3)
-    day_care = DayCare.new("The Dog Spot")
-    day_care.add_customer(joel)
-    day_care.add_customer(billy)
-    assert_equal joel, day_care.customer_by_id(2)
+      expect(day_care.unfed_pets).to eq([samson, molly])
+    end
   end
 end
